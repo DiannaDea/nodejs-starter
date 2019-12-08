bc89f83219a6c6d525e5c2776346167617a197b0
Diana Baburina <dbaburina.consultant@newscorp.com>
2019-12-08 21:35:04 +0200

    login validation

--- client/src/components/CreateUserForm/CreateUserForm.container.js
+++ client/src/components/CreateUserForm/CreateUserForm.container.js
@@ -3,9 +3,18 @@ import CreateUserFormView from './CreateUserForm.view'
 
 class CreateUserForm extends React.Component {
   state = {
-    login: '',
-    password: '',
-    fullName: '',
+    login: {
+      text: '',
+      isValid: false
+    },
+    password: {
+      text: '',
+      isValid: false,
+    },
+    fullName: {
+      text: '',
+      isValid: false
+    },
     groupIds: []
   }
 
@@ -16,8 +25,17 @@ class CreateUserForm extends React.Component {
   }
 
   handleInputChange = (inputType, value) => {
+    const validations = {
+      login: (value) => value && value.length >= 5,
+      password: (value) => value && value.length >= 3,
+      fullName: (value) => value && value.length >= 1,
+    }
+
     this.setState({
-      [inputType]: value
+      [inputType]: {
+        text: value,
+        isValid: validations[inputType](value)
+      }
     })
   }
 
@@ -35,21 +53,29 @@ class CreateUserForm extends React.Component {
 
   handleUserCreate = () => {
     const { createUser } = this.props;
+    const { login, password, fullName } = this.state
 
-    createUser({
-      login: this.state.login,
-      password: this.state.password,
-      fullName: this.state.fullName,
-      groupIds: this.state.groupIds,
-    })
+    if (login.isValid && password.isValid && fullName.isValid) {
+      createUser({
+        login: login.text,
+        password: password.text,
+        fullName: fullName.text,
+        groupIds: this.state.groupIds,
+      });
+    }
   }
 
   render() {
+    console.log('==this.state', this.state);
+
     return (
       <CreateUserFormView
         handleInputChange={this.handleInputChange}
         handleCheckboxSelect={this.handleCheckboxSelect}
         handleCreateUser={this.handleUserCreate}
+        loginInputValid={this.state.login.isValid}
+        passwordInputValid={this.state.password.isValid}
+        fullNameInputValid={this.state.fullName.isValid}
       />
     )
   }
--- client/src/components/CreateUserForm/CreateUserForm.view.jsx
+++ client/src/components/CreateUserForm/CreateUserForm.view.jsx
@@ -1,35 +1,50 @@
 import React from 'react'
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, FormFeedback } from 'reactstrap';
 
-const CreateUserForm = ({ handleInputChange, handleCheckboxSelect, handleCreateUser }) => {
+const CreateUserForm = (props) => {
+  const {
+    handleInputChange,
+    handleCheckboxSelect,
+    handleCreateUser,
+    loginInputValid,
+    passwordInputValid,
+    fullNameInputValid
+  } = props;
+
   return (
     <Form>
       <FormGroup>
         <Label for="login">Login</Label>
         <Input
+          {...(loginInputValid) ? {valid: true} : {invalid: true }} 
           type="text"
           name="login"
           id="login"
           onChange={(e) => handleInputChange('login', e.target.value)}
         />
+        <FormFeedback>Login must contain at least 5 symbols</FormFeedback>
       </FormGroup>
       <FormGroup>
         <Label for="password">Password</Label>
         <Input
+          {...(passwordInputValid) ? {valid: true} : {invalid: true }} 
           type="text"
           name="password"
           id="password"
           onChange={(e) => handleInputChange('password', e.target.value)}
         />
+        <FormFeedback>Password must contain at least 3 symbols</FormFeedback>
       </FormGroup>
       <FormGroup>
         <Label for="fullName">Full name</Label>
         <Input
+          {...(fullNameInputValid) ? {valid: true} : {invalid: true }} 
           type="text"
           name="fullName"
           id="fullName"
           onChange={(e) => handleInputChange('fullName', e.target.value)}
         />
+        <FormFeedback>Full name is required</FormFeedback>
       </FormGroup>
       <FormGroup check>
         <Label check>
